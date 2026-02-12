a692c185567f3d644f5a00c6e6118b9e3ab2ea30
keyrun <kiranpatil1999@outlook.com>
2026-02-12 08:15:51 +0530

    search in an array

--- main/java/org/example/BinarySearch/BinareySearch.java
+++ main/java/org/example/BinarySearch/BinareySearch.java
@@ -57,5 +57,26 @@ public class BinareySearch
 
         return ans;
     }
+    //Given a sorted array of integers nums with 0-based indexing, find the index of a specified target integer. If the target is found in the array, return its index. If the target is not found, return -1.
+    //Example 1
+    //Input: nums = [-1,0,3,5,9,12], target = 9
+    //Output: 4
+    //Explanation: The target integer 9 exists in nums and its index is 4
+    //Example 2
+    //Input: nums = [-1,0,3,5,9,12], target = 2
+    //Output: -1
+    //Explanation: The target integer 2 does not exist in nums so return -1
+    public int search(int[] arr, int target)
+    {
+        int n = arr.length;
+        int low = 0, high = n - 1;
 
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) return mid;
+            else if (target > arr[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
 }
--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -6,8 +6,11 @@ public class Main {
     private static BinareySearch binarySearch = new BinareySearch();
     static void main()
     {
-        int[] arr = {10, 20, 30, 30,30,30,30,40,40,40, 50};
-        System.out.println("Last Occurance " + binarySearch.lastOccurrence(arr,30));
-        System.out.println("First Occurance " + binarySearch.firstOccurrence(arr,30));
+        int[] firstLastOccoranceArr = {10, 20, 30, 30,30,30,30,40,40,40, 50};
+        int[] searchArr = {3, 4, 6, 7, 9, 12, 16, 17};
+        System.out.println("Last Occurance " + binarySearch.lastOccurrence(firstLastOccoranceArr,30));
+        System.out.println("First Occurance " + binarySearch.firstOccurrence(firstLastOccoranceArr,30));
+        int target = 9;
+        System.out.println("Serached " + target+" in array using BinarySearch at index  " + binarySearch.search(searchArr,9));
     }
 }
