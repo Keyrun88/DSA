b89ae0d04dbe8cba910726d71659f59fd4e43f18
keyrun <kiranpatil1999@outlook.com>
2026-03-05 17:17:54 +0530

    graph word search problem

---
+++ main/java/org/example/Graph/WordSearch.java
@@ -0,0 +1,43 @@
+package org.example.Graph;
+
+public class WordSearch
+{
+    public boolean exist(char[][] board, String word) {
+
+        int n = board.length;
+        int m = board[0].length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(dfs(board, word, i, j, 0))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    private  static boolean  dfs(char[][] board,String word,int i,int j,int index)
+    {
+        if(word.length() == index) return  true;
+        if(i<0 || j<0 || i>=board.length || j>=board[0].length || board[i][j] != word.charAt(index))
+            return false;
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean found =
+        WordSearch.dfs(board,word,i+1,j,index+1) ||
+        WordSearch.dfs(board,word,i-1,j,index+1) ||
+        WordSearch.dfs(board,word,i,j+1,index+1) ||
+        WordSearch.dfs(board,word,i,j-1,index+1);
+
+        board[i][j] = temp;
+
+        return found;
+
+    }
+}
+
+
--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -1,16 +1,24 @@
 package org.example;
 
 import org.example.BinarySearch.BinareySearch;
+import org.example.Graph.WordSearch;
 
 public class Main {
     private static BinareySearch binarySearch = new BinareySearch();
+    private static WordSearch wordSearch  = new WordSearch();
     static void main()
     {
-        int[] firstLastOccoranceArr = {10, 20, 30, 30,30,30,30,40,40,40, 50};
-        int[] searchArr = {3, 4, 6, 7, 9, 12, 16, 17};
-        System.out.println("Last Occurance " + binarySearch.lastOccurrence(firstLastOccoranceArr,30));
-        System.out.println("First Occurance " + binarySearch.firstOccurrence(firstLastOccoranceArr,30));
-        int target = 9;
-        System.out.println("Serached " + target+" in array using BinarySearch at index  " + binarySearch.search(searchArr,9));
+       // int[] firstLastOccoranceArr = {10, 20, 30, 30,30,30,30,40,40,40, 50};
+       // int[] searchArr = {3, 4, 6, 7, 9, 12, 16, 17};
+        //System.out.println("Last Occurance " + binarySearch.lastOccurrence(firstLastOccoranceArr,30));
+       // System.out.println("First Occurance " + binarySearch.firstOccurrence(firstLastOccoranceArr,30));
+       // int target = 9;
+       // System.out.println("Serached " + target+" in array using BinarySearch at index  " + binarySearch.search(searchArr,9));
+
+        // Graph Problems
+
+        char[][] board = {{'A','B','C'},{'D','E','F'},{'M','T','Z'}};
+        String word = "ADET";
+        System.out.println(wordSearch.exist(board,word) ? "Exist" : "Not Exist") ;
     }
 }
